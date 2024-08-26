2285147684c21cfc80e465860d723f3aa57c1454
arthur.salahutdinov <artes_93@mail.ru>
2024-08-27 01:20:48 +0300

    Обновил Main.java с новыми значениями стоимости билета

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 9875;  // измененная стоимость билета
+        int milesPerRuble = 20;   // количество рублей за одну милю
+
+        int bonusMiles = ticketPrice / milesPerRuble;  // расчет бонусных миль
+
+        System.out.println("Количество бонусных миль: " + bonusMiles);
+    }
 }
